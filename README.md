e44f80295a427a62d0bf2b1b2697445fe7384de4
Skyler Lipthay <sl@skylerlipthay.com>
2019-05-11 07:41:49 -0600

    Initialize the repository

---
+++ README.md
@@ -0,0 +1,143 @@
+# Survive
+
+Survive is an experimental implementation of [system
+prevalence](https://en.wikipedia.org/wiki/System_prevalence), an extremely
+simple model for object persistence. It works by *snapshotting your entire data
+occasionally* and maintaining an *append-only journal* of data mutations
+(commonly known as *transactions*) in the meantime.
+
+In many practical applications, system prevalence can replace the need for
+full-fledged databases of all sorts (key-value, relational, etc.).
+
+All you need:
+
+* A plain old Rust data type that is:
+  * Serializable using [Serde](http://serde.rs/).
+  * Only modifiable through serializable mutations.
+* A blank directory on your file system.
+
+That's it! Here's what you don't have to deal with:
+
+* Dedicated server processes
+* Special setup
+* Proprietary query language
+* Constraints on the algorithms, data structures, or indexes you use
+* Limited constraint models (triggers, foreign keys, etc.)
+
+All of your domain logic is implemented in vanilla Rust. All you have to do is
+make sure your data is serializable and your data mutations are well-defined and
+deterministic!
+
+There are some catches, of course:
+
+* Most notably, the entire data must fit in RAM.
+* There is no "schema" abstraction, so there is no e.g. `CREATE TABLE` or `ALTER
+  TABLE` (from SQL). As such, systematic data type migration is currently
+  outside the scope of this library. There are probably ways to help alleviate
+  this practical pain point, but I haven't researched them myself.
+* For a plethora of other pitfalls and trade-offs, see [further
+  reading](#further-reading).
+
+For a technical explanation of Survive, please refer to the source code's
+documentation. The code is lightweight and the details are simple.
+
+## Full example
+
+```toml
+[dependencies]
+survive = "0.1"
+serde = "1.0"
+serde_derive = "1.0"
+```
+
+```rust
+extern crate serde;
+#[macro_use] extern crate serde_derive;
+extern crate survive;
+
+use survive::{Survive, Survivable, Mutation};
+use std::collections::BTreeSet;
+
+#[derive(Default, Deserialize, Serialize)]
+struct Model {
+    // You can use whatever data structure you want—as long as it's serializable!
+    values: BTreeSet<String>,
+}
+
+impl Survivable for Model {
+    type Mutation = ModelMutation;
+}
+
+// The master data mutation structure.
+//
+// Warning: Serialization of enum variants are not guaranteed to be stable...
+// This mutation interface needs some work.
+#[derive(Deserialize, Serialize)]
+enum ModelMutation {
+    // Add a value to the set.
+    Add(String),
+    // Remove a value to the set.
+    Remove(String),
+}
+
+impl Mutation<Model> for ModelMutation {
+    // The implementation of this function **must be deterministic**!
+    fn mutate(&self, data: &mut Model) {
+        match *self {
+            ModelMutation::Add(ref value) => { data.values.insert(value.clone()); },
+            ModelMutation::Remove(ref value) => { data.values.remove(value); },
+        }
+    }
+}
+
+fn main() {
+    // Create a new home for some data (assuming the specified directory does not yet exist):
+    let mut data = Survive::<Model>::new("path/to/some-directory").unwrap();
+    data.mutate(&FooMutation::Add("Hello!".to_string())).unwrap();
+    data.mutate(&FooMutation::Add("World!".to_string())).unwrap();
+    assert_eq!(data.get().values.contains("Hello!"));
+    data.mutate(&FooMutation::Remove("Hello!".to_string())).unwrap();
+    // The system is closed on drop:
+    drop(data);
+
+    // Open the persisted data again:
+    let data = Survive::<Model>::new("path/to/some-directory").unwrap();
+    assert_eq!(!data.get().values.contains("Hello!"));
+    assert_eq!(data.get().values.contains("World!"));
+}
+```
+
+## Performance
+
+Some quick numbers: On my reasonably powered development computer I am able to
+create and persist a `BTreeSet<String>` with 1,000,000 individually-added
+strings in ~700ms, where each string is approximately 6 bytes long like in the
+above example. The resultant data directory is ~20 MB in size. Re-loading the
+persisted data takes about ~400ms.
+
+The current architecture seems to scale reasonably well to data approaching 1
+GB, but some performance tuning (see `survive::Options`) is necessary. Most
+problematic is the automatic "compaction" (full-data snapshotting) that Survive
+does by default, which occurs after a certain number of journaled mutations and
+blocks execution. There's a trade-off here between 1) start-up time and journal
+file length, and 2) occasional runtime pauses to save data snapshots.
+
+In my view, the strongest benefit of this architecture is that data reads are
+virtually free of overhead—you're just accessing plain-old Rust data structures.
+As such, the analogous ["full table
+scans"](https://en.wikipedia.org/wiki/Full_table_scan) are not nearly so scary,
+and data indexes can be catered exactly to your needs.
+
+## Further reading
+
+System prevalence has been around since at least 2001 with Klaus Wuestefeld's
+[Prevayler](http://prevayler.org/), an implementation of the pattern in Java.
+The simplicity of this architectural model is quite appealing, but there are
+many trade-offs to consider when adopting it. Unfortunately there's a shocking
+amount of dogmatism on both sides, which can be reviewed on the legendary
+[wiki.c2.com](http://wiki.c2.com/?PrevalenceLayer). Even if the discussion is
+quite old, most of the points seem to me to still be relevant today.
+
+* [Object Prevalence: An In-Memory, No-Database Solution to
+  Persistence](https://medium.com/@paul_83250/object-prevalence-an-in-memory-no-database-solution-to-persistence-a1ebcd1493b0)
+* [Prevayler's old wiki](http://prevayler.org/old_wiki/Welcome.html)
---
+++ src/lib.rs
@@ -0,0 +1,286 @@
+extern crate bincode;
+extern crate byteorder;
+extern crate serde;
+
+use byteorder::{LittleEndian, ReadBytesExt, WriteBytesExt};
+use serde::{de::DeserializeOwned, Serialize};
+use std::{
+    fs::{self, File},
+    io::{self, Read, Write, BufReader, BufWriter},
+    path::{Path, PathBuf},
+};
+
+/// A representation of a persistable data type.
+///
+/// All you need:
+///
+/// * A data type that is:
+///   * Serializable/deserializable through Serde. If the type or its serialization implementation
+///     changes, it is your responsibility to migrate your data to the new type. `Survive`'s will
+///     fail if there is a serialization mismatch.
+///   * Only modifiable through serializable/deserializable mutations. You need the discipline to
+///     not subvert this constraint (via interior mutability, inconsistent serialization,
+///     non-deterministic behavior, etc.), otherwise you will likely end up with invalid data
+///     mutations and thus inconsistent/unexpected data. Like the main data type, mutation types
+///     must also be consistently serializable.
+/// * A blank directory on your file system. Do not manually modify the contents of this directory!
+///   But feel free to move, copy, or delete it as needed.
+///
+/// # Technical description
+///
+/// ## Files
+///
+/// There are at most three files in the persistence directory at any given time:
+///
+/// * **State file**: This is the file that contains the a complete serialization of the data at
+///   some point.
+/// * **Journal file**: This file contains a serialized list of mutations made to the data since the
+///   time of the state file's creation.
+/// * **Transitional state file**: This file exists temporarily during the journal compaction phase,
+///   and is meant to replace the state file once compaction is complete.
+///
+/// ## Compaction
+///
+/// In order to prevent the journal file from indefinitely growing in length, it is occasionally
+/// cleared and the state file is recreated from scratch during the **compaction phase**. This phase
+/// is trigged when the journal file exceeds 10 MB, and also occurs during startup (regardless of
+/// journal file size).
+///
+/// While compaction is occurring, a write lock is placed on the data so that intermediate data
+/// modifications are not at risk of not being recorded in the journal. Hopefully this locking
+/// period is not a considerable burden. Alternatively, locking could be prevented by creating a
+/// temporary clone of the data in memory so that mutations of the main data can continue
+/// uninterrupted. In this case, a transitional journal is also necessary. The costs of this method
+/// are: 1) the data type has to implement `Clone`, and 2) twice the memory is required.
+pub struct Survive<T: Survivable> {
+    path: PathBuf,
+    data: T,
+    journal: BufWriter<File>,
+    journal_file_length: usize,
+    options: Options,
+}
+
+impl<T: Survivable> Survive<T> {
+    pub fn new<P: AsRef<Path>>(path: P) -> Result<Survive<T>, Error> {
+        Self::with_options(path, Options::default())
+    }
+
+    /// Opens and synchronizes with a persisted instance of the data type `T`. If a persistence does
+    /// not yet exist, it is created using the `Default` implementation of `T`.
+    ///
+    /// The given `path` is a path to a directory that will contain all of the necessary files to
+    /// persist the data. If the directory or any of its parents do not exist, they will be
+    /// recursively created.
+    pub fn with_options<P: AsRef<Path>>(path: P, options: Options) -> Result<Survive<T>, Error> {
+        let path = path.as_ref().to_path_buf();
+        let data = Self::load(path.as_ref())?.unwrap_or_else(|| T::default());
+        let journal_path = path.join("journal");
+        let journal_file = fs::OpenOptions::new().append(true).create(true).open(&journal_path)?;
+        let journal = BufWriter::new(journal_file);
+        let mut result = Survive { path, data, journal, journal_file_length: 0, options };
+        result.compact()?;
+        Ok(result)
+    }
+
+    /// Returns an immutable reference to the underlying data.
+    pub fn get(&self) -> &T {
+        &self.data
+    }
+
+    /// Performs a mutation on the underlying data.
+    pub fn mutate<M: Mutation<T>>(&mut self, mutation: &M) -> Result<(), Error> {
+        fn write_buf(w: &mut Write, buf: &[u8]) -> Result<(), Error> {
+            w.write_u32::<LittleEndian>(buf.len() as u32)?;
+            w.write_all(buf.as_ref())?;
+            Ok(())
+        }
+
+        mutation.mutate(&mut self.data);
+        let buf = bincode::serialize(&mutation)?;
+
+        let write_result = if self.options.use_journal_buffer {
+            write_buf(&mut self.journal, buf.as_ref())
+        } else {
+            write_buf(&mut self.journal, buf.as_ref()).and_then(|_| Ok(self.journal.flush()?))
+        };
+        // If writing fails, the journal file may be corrupted and compaction should be triggered
+        // immediately.
+        if write_result.is_err() {
+            return self.compact();
+        }
+
+        self.journal_file_length += 4 + buf.len();
+        if let Some(max) = self.options.max_journal_file_length {
+            if self.journal_file_length > max {
+                self.compact()?;
+            }
+        }
+        Ok(())
+    }
+
+    /// Returns the current length of the journal file in bytes.
+    pub fn journal_file_length(&self) -> usize {
+        self.journal_file_length
+    }
+
+    // Force a compaction of the journal file into the state file.
+    pub fn compact(&mut self) -> Result<(), Error> {
+        let state_path = self.path.join("state");
+        let transitional_state_path = self.path.join("state~");
+
+        let mut transitional_state = BufWriter::new(File::create(&transitional_state_path)?);
+        bincode::serialize_into(&mut transitional_state, &self.data)?;
+
+        if state_path.exists() {
+            fs::remove_file(&state_path)?;
+        }
+
+        self.journal.flush()?;
+        self.journal.get_ref().set_len(0)?;
+        self.journal_file_length = 0;
+
+        fs::rename(&transitional_state_path, &state_path)?;
+
+        Ok(())
+    }
+
+    fn load(path: &Path) -> Result<Option<T>, Error> {
+        fs::create_dir_all(path)?;
+
+        let state_path = path.join("state");
+        let journal_path = path.join("journal");
+        let transitional_state_path = path.join("state~");
+
+        if !state_path.exists() {
+            if journal_path.exists() {
+                // The program previously crashed right after deleting the main state file and right
+                // before deleting the journal file.
+                fs::remove_file(&journal_path)?;
+            }
+
+            if transitional_state_path.exists() {
+                // The program previously crashed right after deleting the main state file (and the
+                // journal file) and right before renaming the transitional state file.
+                fs::rename(&transitional_state_path, &state_path)?;
+            } else {
+                return Ok(None);
+            }
+        }
+
+        if transitional_state_path.exists() {
+            fs::remove_file(&transitional_state_path)?;
+        }
+
+        let mut data = bincode::deserialize_from(BufReader::new(File::open(state_path)?))?;
+        if journal_path.exists() {
+            let mut journal = BufReader::new(File::open(journal_path)?);
+            loop {
+                let length = match journal.read_u32::<LittleEndian>() {
+                    Ok(length) => length as usize,
+                    Err(err) => {
+                        if let io::ErrorKind::UnexpectedEof = err.kind() {
+                            // The journal file is finished, or the program previously crashed in
+                            // the middle of writing the chunk length.
+                            break;
+                        } else {
+                            return Err(err.into());
+                        }
+                    },
+                };
+
+                let mut buf = vec![0; length];
+                if let Err(err) = journal.read_exact(buf.as_mut_slice()) {
+                    if let io::ErrorKind::UnexpectedEof = err.kind() {
+                        // The program previously crashed in the middle of writing the chunk.
+                        break;
+                    }
+                }
+
+                let mutation: T::Mutation = bincode::deserialize(&buf)?;
+                mutation.mutate(&mut data);
+            }
+        }
+
+        Ok(Some(data))
+    }
+}
+
+/// A type that can be persisted by `Survive`.
+pub trait Survivable: Default + Serialize + DeserializeOwned {
+    type Mutation: Mutation<Self>;
+}
+
+/// A serializable change to the `Survivable` data.
+pub trait Mutation<T: Survivable>: Serialize + DeserializeOwned {
+    /// Makes a change to the data.
+    ///
+    /// # Determinism
+    ///
+    /// It is up to the programmer to **make sure that this implementation is side effect free**,
+    /// i.e. **deterministic**. Executions of the function must produce the exact same data mutation
+    /// each time.
+    ///
+    /// Common accidental violations of this rule include:
+    ///
+    /// * Depending on external resources such as files or network endpoints
+    /// * Generating dates or timestamps in the function
+    /// * Allocating random numbers or IDs
+    ///
+    /// These violations produce different results on subsequent "replays" (i.e. when the journal
+    /// file is processed).
+    fn mutate(&self, data: &mut T);
+}
+
+/// Settings for tweaking the performance of a `Survive` instance.
+pub struct Options {
+    /// The limit of the journal file length (in bytes). When the length exceeds this value,
+    /// `Survive` will automatically compact the state file and clear the journal.
+    ///
+    /// If this is `None`, automatic compaction is disabled. If this is `Some(0)`, compaction runs
+    /// after every data mutation.
+    ///
+    /// By default, this is set to 10 MB (10,485,760 bytes).
+    pub max_journal_file_length: Option<usize>,
+    /// By default, Writing to the journal file is buffered (via `BufWriter`). This improves
+    /// mutation performance significantly (in some experiments, by approximately an order of
+    /// magnitude), but comes at a disadvantage. Because serialized mutations are not flushed to the
+    /// journal file immediately, abnormal program closure can cause the mutations to not be
+    /// journaled and thus lost forever.
+    ///
+    /// At the time of writing, we use `BufWriter`'s default of an 8 KB buffer. So, for reasonably
+    /// small transactions (in terms of their serialized bytes), a crash can result in approximately
+    /// 8 KB of transactions to be lost. In the case of a large transaction that exceeds 8 KB in
+    /// serialized size, the entire large transaction can be lost.
+    ///
+    /// To disable journal file write buffering, set this to `false`.
+    pub use_journal_buffer: bool,
+}
+
+impl Default for Options {
+    fn default() -> Options {
+        Options {
+            max_journal_file_length: Some(10_485_760),
+            use_journal_buffer: true,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    /// A serialization or deserialization error.
+    Bincode(bincode::Error),
+    /// A system I/O error.
+    Io(io::Error),
+}
+
+impl From<bincode::Error> for Error {
+    fn from(err: bincode::Error) -> Error {
+        Error::Bincode(err)
+    }
+}
+
+impl From<io::Error> for Error {
+    fn from(err: io::Error) -> Error {
+        Error::Io(err)
+    }
+}
